9b88f18b81c4c2cb8e16d2746884095a8a37fc01
satyasundar <satyasundar.nayak@gmail.com>
2024-04-20 05:31:00 +0530

    collectible workshop

---
+++ pallets/collectibles/src/lib.rs
@@ -0,0 +1,43 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet(dev_mode)]
+pub mod pallet {
+	use frame_support::{pallet_prelude::*, traits::{Currency, Randomness}, sp_runtime::traits::BlockNumberProvider};
+	use frame_system::pallet_prelude::*;
+	
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Currency: Currency<Self::AccountId>;
+		type CollectionRandomness: Randomness<Self::Hash, BlockNumberFor<Self>>;
+
+		#[pallet::constant]
+		type MaximumOwned: Get<u32>;
+	}
+
+	#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	pub enum Color {
+		Red,
+		Yellow,
+		Blue,
+		Green
+	}
+
+	type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+	#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	pub struct Collectible<T: Config> {
+		pub unique_id: [u8; 16],
+		pub price: Option<BalanceOf<T>>,
+		pub color: Color,
+		pub owner: T::AccountId,
+	}
+]
+}
